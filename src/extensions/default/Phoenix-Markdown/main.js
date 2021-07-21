612f6580cc7aed257a0cbf4acbbed615ee67e9eb
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2021-07-21 17:26:15 +0000

    chore(package): update istanbul to version 0.4.5

--- src/extensions/default/Phoenix-Markdown/main.js
+++ src/extensions/default/Phoenix-Markdown/main.js
@@ -436,4 +436,118 @@ define(function (require, exports, module) {
         description: Strings.DESCRIPTION_MAX_LINE_LENGTH
     });
 
-    var BA
+    var BAR_COMMAND_ID = "alanhohn.togglemarkdownbar",
+        H1_COMMAND_ID = "alanhohn.markdownheading1",
+        H2_COMMAND_ID = "alanhohn.markdownheading2",
+        H3_COMMAND_ID = "alanhohn.markdownheading3",
+        H4_COMMAND_ID = "alanhohn.markdownheading4",
+        BOLD_COMMAND_ID = "alanhohn.markdownbold",
+        ITALIC_COMMAND_ID = "alanhohn.markdownitalic",
+        STRIKE_COMMAND_ID = "alanhohn.markdownstrike",
+        CODE_COMMAND_ID = "alanhohn.markdowncode",
+        BULLET_COMMAND_ID = "alanhohn.markdownbullet",
+        NUMBERED_COMMAND_ID = "alanhohn.markdownnumbered",
+        QUOTE_COMMAND_ID = "alanhohn.markdownquote",
+        CODEBLOCK_COMMAND_ID = "alanhohn.markdowncodeblock",
+        PARAGRAPH_COMMAND_ID = "alanhohn.markdownparagraph",
+        REFLOW_COMMAND_ID = "alanhohn.markdownreflow";
+
+    cmdToolbar = CommandManager.register(Strings.MENU_TOOLBAR, BAR_COMMAND_ID, toggleBar);
+
+    CommandManager.register(Strings.HINT_H1, H1_COMMAND_ID, Handler.h1);
+    CommandManager.register(Strings.HINT_H2, H2_COMMAND_ID, Handler.h2);
+    CommandManager.register(Strings.HINT_H3, H3_COMMAND_ID, Handler.h3);
+    CommandManager.register(Strings.HINT_H4, H4_COMMAND_ID, Handler.h4);
+    CommandManager.register(Strings.HINT_BOLD, BOLD_COMMAND_ID, Handler.bold);
+    CommandManager.register(Strings.HINT_ITALIC, ITALIC_COMMAND_ID, Handler.italic);
+    CommandManager.register(Strings.HINT_STRIKE, STRIKE_COMMAND_ID, Handler.strikethrough);
+    CommandManager.register(Strings.HINT_CODE, CODE_COMMAND_ID, Handler.code);
+    CommandManager.register(Strings.HINT_BULLET, BULLET_COMMAND_ID, Handler.bullet);
+    CommandManager.register(Strings.HINT_NUMBERED, NUMBERED_COMMAND_ID, Handler.numbered);
+    CommandManager.register(Strings.HINT_QUOTE, QUOTE_COMMAND_ID, Handler.quote);
+    CommandManager.register(Strings.HINT_CODEBLOCK, CODEBLOCK_COMMAND_ID, Handler.codeblock);
+    CommandManager.register(Strings.HINT_PARAGRAPH, PARAGRAPH_COMMAND_ID, Handler.paragraph);
+    CommandManager.register(Strings.HINT_REFLOW, REFLOW_COMMAND_ID, Handler.reflow);
+
+    KeyBindingManager.addBinding(H1_COMMAND_ID, KeyboardPrefs.heading1);
+    KeyBindingManager.addBinding(H2_COMMAND_ID, KeyboardPrefs.heading2);
+    KeyBindingManager.addBinding(H3_COMMAND_ID, KeyboardPrefs.heading3);
+    KeyBindingManager.addBinding(H4_COMMAND_ID, KeyboardPrefs.heading4);
+    KeyBindingManager.addBinding(BOLD_COMMAND_ID, KeyboardPrefs.bold);
+    KeyBindingManager.addBinding(ITALIC_COMMAND_ID, KeyboardPrefs.italic);
+    KeyBindingManager.addBinding(STRIKE_COMMAND_ID, KeyboardPrefs.strikethrough);
+    KeyBindingManager.addBinding(CODE_COMMAND_ID, KeyboardPrefs.code);
+    KeyBindingManager.addBinding(BULLET_COMMAND_ID, KeyboardPrefs.bullet);
+    KeyBindingManager.addBinding(NUMBERED_COMMAND_ID, KeyboardPrefs.numbered);
+    KeyBindingManager.addBinding(QUOTE_COMMAND_ID, KeyboardPrefs.quote);
+    KeyBindingManager.addBinding(CODEBLOCK_COMMAND_ID, KeyboardPrefs.codeblock);
+    KeyBindingManager.addBinding(PARAGRAPH_COMMAND_ID, KeyboardPrefs.paragraph);
+    KeyBindingManager.addBinding(REFLOW_COMMAND_ID, KeyboardPrefs.reflow);
+
+    ExtensionUtils.loadStyleSheet(module, "styles/styles.css");
+    ExtensionUtils.loadStyleSheet(module, "styles/octicons.css");
+    barShouldShow = true;
+
+    activeEditorChangeHandler(null, EditorManager.getActiveEditor(), null);
+    EditorManager.on("activeEditorChange", activeEditorChangeHandler);
+
+
+    // Debounce event callback to avoid excess overhead
+    // Update preview 300 ms ofter document change
+    // Sync scroll 1ms after document scroll (just enough to ensure
+    // the document scroll isn't blocked).
+    _documentChange = _.debounce(_documentChange, 30);
+    _editorScroll = _.debounce(_editorScroll, 1);
+
+    // Insert CSS for this extension
+    ExtensionUtils.loadStyleSheet(module, "styles/MarkdownPreview.css");
+
+    // Add toolbar icon
+
+    $icon = $("<a>")
+        .attr({
+            id: "markdown-preview-icon",
+            href: "#"
+        })
+        .css({
+            display: "none"
+        })
+        .click(_toggleVisibility)
+        .appendTo($("#main-toolbar .buttons"));
+
+
+
+    // Add a document change handler
+    MainViewManager.on("currentFileChange", _currentDocChangedHandler);
+
+    toggleCmd = CommandManager.register("Markdown Preview", "toggleMarkdownPreview", _toggleVisibility);
+    toggleCmd.setChecked(realVisibility);
+    toggleCmd.setEnabled(realVisibility);
+
+    function _createPanel() {
+        $panel = $(panelHTML);
+        $iframe = $panel.find("#panel-markdown-preview-frame");
+        let minSize = window.innerWidth/3;
+
+        panel = WorkspaceManager.createPluginPanel("markdown-preview-panel", $panel, minSize, $icon);
+
+        WorkspaceManager.recomputeLayout(false);
+        $settingsToggle = $("#markdown-settings-toggle")
+            .click(function (e) {
+                if ($settings) {
+                    _hideSettings();
+                } else {
+                    _showSettings(e);
+                }
+            });
+    }
+
+    // currentDocumentChange is *not* called for the initial document. Use
+    // appReady() to set initial state.
+    AppInit.appReady(function () {
+        _createPanel();
+        _currentDocChangedHandler();
+    });
+});
+
+
