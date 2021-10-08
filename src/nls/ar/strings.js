3d6eadf199ef8ca5ffa92914702dfaec76d6d388
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2021-10-08 06:00:58 +0000

    Complete Spanish translation

--- src/nls/ar/strings.js
+++ src/nls/ar/strings.js
@@ -1,4 +1,25 @@
-{
+
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+define({
   "SYSTEM_DEFAULT": "النظام الافتراضي",
   "PROJECT_BUSY": "عمليات المشروع قيد التنفيذ",
   "DUPLICATING": "تكرار {0}",
@@ -840,4 +861,4 @@
   "BEAUTIFY_OPTION_SINGLE_ATTRIBUTE_PER_LINE": "فرض سمة واحدة لكل سطر في HTML و Vue و JSX",
   "BEAUTIFY_OPTION_PROSE_WRAP": "لف النثر إذا تجاوز عرض الطباعة في ملفات مارك داون",
   "BEAUTIFY_OPTION_PRINT_TRAILING_COMMAS": "اطبع الفواصل اللاحقة كلما أمكن ذلك في هياكل نحوية متعددة الأسطر مفصولة بفواصل"
-}
+});
