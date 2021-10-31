26d5a9d0153c6cb393598f2c720ba1c95d5a04c3
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2021-10-31 13:31:23 +0000

    change email

---
+++ src/nls/fr/strings.js
@@ -0,0 +1,72 @@
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+define({
+  "SYSTEM_DEFAULT": "Langue par défaut du système",
+  "PROJECT_BUSY": "Opérations du projet en cours",
+  "DUPLICATING": "Dupliquer {0}",
+  "MOVING": "Déménagement {0}",
+  "COPYING": "Copie de {0}",
+  "DELETING": "Suppression de {0}",
+  "RENAMING": "Renommer",
+  "GENERIC_ERROR": "(Erreur {0})",
+  "NOT_FOUND_ERR": "Fichier/répertoire introuvable.",
+  "NOT_READABLE_ERR": "Impossible de lire le fichier/répertoire.",
+  "EXCEEDS_MAX_FILE_SIZE": "Les fichiers dont la taille excède {0} Mo ne peuvent pas être ouverts dans {APP_NAME}.",
+  "NO_MODIFICATION_ALLOWED_ERR": "Le répertoire cible ne peut pas être modifié.",
+  "NO_MODIFICATION_ALLOWED_ERR_FILE": "Vous n’êtes pas autorisé à effectuer des modifications.",
+  "CONTENTS_MODIFIED_ERR": "Le fichier a été modifié dans une application autre que {APP_NAME}.",
+  "UNSUPPORTED_ENCODING_ERR": "Format de codage inconnu",
+  "ENCODE_FILE_FAILED_ERR": "{APP_NAME} n’a pas pu coder le contenu du fichier.",
+  "DECODE_FILE_FAILED_ERR": "{APP_NAME} n’a pas pu décoder le contenu du fichier.",
+  "UNSUPPORTED_UTF16_ENCODING_ERR": "Actuellement, {APP_NAME} ne prend pas en charge les fichiers texte codés UTF-16.",
+  "FILE_EXISTS_ERR": "Le fichier ou le répertoire existe déjà.",
+  "FILE": "fichier",
+  "FILE_TITLE": "Fichier",
+  "DIRECTORY": "répertoire",
+  "DIRECTORY_TITLE": "Répertoire",
+  "DIRECTORY_NAMES_LEDE": "noms de répertoires",
+  "FILENAMES_LEDE": "noms de fichiers",
+  "FILENAME": "Nom de fichier",
+  "DIRECTORY_NAME": "Nom du répertoire",
+  "ERROR_LOADING_PROJECT": "Erreur lors du chargement du projet",
+  "OPEN_DIALOG_ERROR": "Une erreur s’est produite lors de l’affichage de la boîte de dialogue Ouvrir. (Erreur {0})",
+  "REQUEST_NATIVE_FILE_SYSTEM_ERROR": "Une erreur s’est produite lors de la tentative de chargement du répertoire <span class='dialog-filename'>{0}</span>. (Erreur {1})",
+  "READ_DIRECTORY_ENTRIES_ERROR": "Une erreur s’est produite lors de la lecture du contenu du répertoire <span class='dialog-filename'>{0}</span>. (Erreur {1})",
+  "ERROR_OPENING_FILE_TITLE": "Erreur lors de l’ouverture du fichier",
+  "ERROR_OPENING_FILE": "Une erreur s’est produite lors de la tentative d’ouverture du fichier <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_OPENING_FILES": "Une erreur s’est produite en tentant d’ouvrir les fichiers suivants :",
+  "ERROR_RELOADING_FILE_TITLE": "Erreur lors du rechargement des modifications à partir du disque",
+  "ERROR_RELOADING_FILE": "Une erreur s’est produite lors de la tentative de rechargement du fichier <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_SAVING_FILE_TITLE": "Erreur lors de l’enregistrement du fichier",
+  "ERROR_SAVING_FILE": "Une erreur s’est produite lors de la tentative d’enregistrement du fichier <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_RENAMING_FILE_TITLE": "Erreur lors du changement de nom du {0}",
+  "ERROR_RENAMING_FILE": "Une erreur s’est produite lors de la tentative de changement de nom du {2} <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_RENAMING_NOT_IN_PROJECT": "Le fichier ou le répertoire ne fait pas partie du projet actuellement ouvert. Or, seuls les fichiers appartenant au projet peuvent être renommés à ce stade.",
+  "ERROR_MOVING_FILE_TITLE": "Erreur lors du déplacement de l’élément {0}",
+  "ERROR_MOVING_FILE": "Une erreur s’est produite lors du déplacement de l’élément {2} <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_MOVING_NOT_IN_PROJECT": "Impossible de déplacer le fichier/dossier, car ils ne font pas partie du projet en cours.",
+  "ERROR_DELETING_FILE_TITLE": "Erreur lors de la suppression du {0}",
+  "ERROR_DELETING_FILE": "Une erreur s’est produite lors de la tentative de suppression du {2} <span class='dialog-filename'>{0}</span>. {1}",
+  "INVALID_FILENAME_TITLE": "{0} non valide",
+  "CANNOT_PASTE_TITLE": "Impossible de coller {0}",
+  "CANNOT_DOWNLOAD_TITLE": "Impossible de télécharger {0}",
+  "ERR_TYPE_DOWNLOAD_FAILED": "Une erreur s&#39;est produite lors du téléchargement de <span class='dialog-filename'>{0}</span>",
+  "ERR_TYPE_PASTE_FAILED": "Une erreur s&#39;est produite lors du collage de <span class='dialog-filename'>{0}</span>
