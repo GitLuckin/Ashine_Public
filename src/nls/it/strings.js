7ad413f71fad0e16575ece406c49fde83c49e3d9
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2021-12-19 09:07:36 +0000

    Accumulated commit of the following from branch 'origin/ready/mra':

---
+++ src/nls/it/strings.js
@@ -0,0 +1,73 @@
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+define({
+  "SYSTEM_DEFAULT": "Preferita dal sistema",
+  "PROJECT_BUSY": "Operazioni di progetto in corso",
+  "DUPLICATING": "Duplicazione {0}",
+  "MOVING": "Spostamento {0}",
+  "COPYING": "Copia {0}",
+  "DELETING": "Eliminazione di {0}",
+  "RENAMING": "Rinominare",
+  "GENERIC_ERROR": "(errore {0})",
+  "NOT_FOUND_ERR": "Impossibile trovare il file o la cartella.",
+  "NOT_READABLE_ERR": "Impossibile leggere il file o la cartella.",
+  "EXCEEDS_MAX_FILE_SIZE": "File oltre {0} MB non possono essere aperti con {APP_NAME}.",
+  "NO_MODIFICATION_ALLOWED_ERR": "La cartella selezionata non può essere modificata.",
+  "NO_MODIFICATION_ALLOWED_ERR_FILE": "Non hai i permessi necessari per effettuare la modifica.",
+  "CONTENTS_MODIFIED_ERR": "Il file è stato modificato fuori {APP_NAME}.",
+  "UNSUPPORTED_ENCODING_ERR": "Attualmente {APP_NAME} supporta solo testo codificato in UTF-8",
+  "ENCODE_FILE_FAILED_ERR": "{APP_NAME} non è stato in grado di codificare il contenuto del file.",
+  "DECODE_FILE_FAILED_ERR": "{APP_NAME} non è stato in grado di decodificare il contenuto del file.",
+  "UNSUPPORTED_UTF16_ENCODING_ERR": "{APP_NAME} attualmente non supporta i file di testo con codifica UTF-16.",
+  "FILE_EXISTS_ERR": "Il file o la cartella è già presente.",
+  "FILE": "file",
+  "FILE_TITLE": "File",
+  "DIRECTORY": "cartella",
+  "DIRECTORY_TITLE": "Cartella",
+  "DIRECTORY_NAMES_LEDE": "nomi delle cartelle",
+  "FILENAMES_LEDE": "nomi dei file",
+  "FILENAME": "nome del file",
+  "DIRECTORY_NAME": "nome della cartella",
+  "ERROR_LOADING_PROJECT": "Errore durante il caricamento del progetto",
+  "OPEN_DIALOG_ERROR": "Errore durante il caricamento della finestra di dialogo per l’apertura del file. (errore {0})",
+  "REQUEST_NATIVE_FILE_SYSTEM_ERROR": "Errore durante il tentativo di caricare la cartella <span class='dialog-filename'>{0}</span>. (errore {1})",
+  "READ_DIRECTORY_ENTRIES_ERROR": "Errore durante la lettura del contenuto della cartella <span class='dialog-filename'>{0}</span>. (errore {1})",
+  "ERROR_OPENING_FILE_TITLE": "Errore durante l’apertura del file",
+  "ERROR_OPENING_FILE": "Errore durante il tentativo di apertura del file <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_OPENING_FILES": "Errore durante il tentativo di apertura dei seguenti file:",
+  "ERROR_RELOADING_FILE_TITLE": "Errore durante il caricamento delle modifiche dal disco",
+  "ERROR_RELOADING_FILE": "Errore durante il tentativo di ricaricare il file <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_SAVING_FILE_TITLE": "Errore durante il salvataggio del file",
+  "ERROR_SAVING_FILE": "Errore durante il tentativo di salvare il file <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_RENAMING_FILE_TITLE": "Errore durante il tentativo di rinominare il file",
+  "ERROR_RENAMING_FILE": "Errore durante il tentativo di rinominare il file <span class='dialog-filename'>{0}</span>. {1}",
+  "ERROR_RENAMING_NOT_IN_PROJECT": "Il file o la directory non fa parte del progetto attualmente aperto. Sfortunatamente, solo i file di progetto possono essere rinominati a questo punto.",
+  "ERROR_MOVING_FILE_TITLE": "Errore durante lo spostamento di {0}",
+  "ERROR_MOVING_FILE": "Si è verificato un errore durante il tentativo di spostare {2} <span class='dialog-filename'>{0}</span> . {1}",
+  "ERROR_MOVING_NOT_IN_PROJECT": "Impossibile spostare il file/la cartella, poiché non fanno parte del progetto corrente.",
+  "ERROR_DELETING_FILE_TITLE": "Errore durante il tentativo di eliminazione del file",
+  "ERROR_DELETING_FILE": "Errore durante il tentativo di eliminazione del file <span class='dialog-filename'>{0}</span>. {1}",
+  "INVALID_FILENAME_TITLE": "Il {0} non è valido",
+  "CANNOT_PASTE_TITLE": "Impossibile incollare {0}",
+  "CANNOT_DOWNLOAD_TITLE": "Impossibile scaricare {0}",
+  "ERR_TYPE_DOWNLOAD_FAILED": "Si è verificato un errore durante il download di <span class='dialog-filename'>{0}</span>",
+  "ERR_TYPE_PASTE_FAILED": "Si è verificato un errore durante l&#39;incollaggio di <span class='dialog-filename'>{0}</span> in <span class='dialog-filename'>{1}</span>",
+  "CANNOT_DUPLIC
