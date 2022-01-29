45f5b389e1fb39d06140e4500e9d17580237d3b9
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2022-01-29 17:46:37 +0000

    adding GA

---
+++ src/nls/sr/strings.js
@@ -0,0 +1,50 @@
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+define({
+  "SYSTEM_DEFAULT": "језик система",
+  "PROJECT_BUSY": "Пројектне операције су у току",
+  "DUPLICATING": "Дуплицирање {0}",
+  "MOVING": "Премештање {0}",
+  "COPYING": "Копирање {0}",
+  "DELETING": "Брисање {0}",
+  "RENAMING": "Преименовање",
+  "GENERIC_ERROR": "(грешка {0})",
+  "NOT_FOUND_ERR": "Непостојећа датотека.",
+  "NOT_READABLE_ERR": "Датотека не може бити учитана.",
+  "EXCEEDS_MAX_FILE_SIZE": "Датотеке веће од {0} MB се не могу отворити у {APP_NAME}-у.",
+  "NO_MODIFICATION_ALLOWED_ERR": "Циљани директоријум не може бити измењен.",
+  "NO_MODIFICATION_ALLOWED_ERR_FILE": "Тренутна овлашћења Вам не дозвољавају да правите измене.",
+  "CONTENTS_MODIFIED_ERR": "Ова датотека је била измењена ван {APP_NAME}-а.",
+  "UNSUPPORTED_ENCODING_ERR": "{APP_NAME} тренутно подржава искључиво текстуалне датотеке које су енкодиране у UTF-8 формату.",
+  "ENCODE_FILE_FAILED_ERR": "{АПП_НАМЕ} није могао да кодира садржај датотеке.",
+  "DECODE_FILE_FAILED_ERR": "{АПП_НАМЕ} није могао да декодира садржај датотеке.",
+  "UNSUPPORTED_UTF16_ENCODING_ERR": "{АПП_НАМЕ} тренутно не подржава текстуалне датотеке кодиране УТФ-16.",
+  "FILE_EXISTS_ERR": "Датотека или директоријум већ постоји.",
+  "FILE": "датотеци",
+  "FILE_TITLE": "Датотека",
+  "DIRECTORY": "директоријум",
+  "DIRECTORY_TITLE": "Директоријум",
+  "DIRECTORY_NAMES_LEDE": "Имена директоријума",
+  "FILENAMES_LEDE": "Имена датотека",
+  "FILENAME": "Име датотеке",
+  "DIRECTORY_NAME": "Име директоријума",
+  "ERROR_LOADING_PROJECT": "Грешка приликом учитавања пројекта",
+  "OPEN_DIALOG_ERROR": "Дошло је до грешке приликом приказивања 
