fc793be0fa9da5e50520354a3f0b47136049dddb
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2022-08-06 08:07:03 +0000

    Update SJ_Simulators.rst

---
+++ test/smokes/browserFSTransfer/CodeFolding/Prefs.js
@@ -0,0 +1,3 @@
+/**
+ * Wrapper around brackets pref system to ensure preferences are stored in in one single object instead of using multiple keys.
+ * This is to make it easy for the user who edits their preferences file to easily manage the potentially numerous lines of 
