2f60f76c160a7d72b3eee85d33f73c68bbddeeb1
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2022-12-07 12:18:25 +0000

    update grammar

---
+++ test/spec/FindReplace-test-files-large/foo6.js
@@ -0,0 +1,14 @@
+
+/* Test comment */
+define(function (require, exports, module) {
+    var Foo = require("modules/Foo"),
+        Bar = require("modules/Bar"),
+        Baz = require("modules/Baz");
+    
+    function callFoo() {
+        
+        foo();
+        
+    }
+
+}
