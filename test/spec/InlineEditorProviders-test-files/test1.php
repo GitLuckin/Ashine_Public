6d5a0190502eb67d2adb9845a9787f2b87db8a67
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2023-01-31 10:43:45 +0000

    jStorage add/update

---
+++ test/spec/InlineEditorProviders-test-files/test1.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style type="text/css">
+{{0}}   body {
+            background: url("<?php echo $img; ?>") no-repeat;
+            background-size: 100% 100%;
+            color: white;
+        }
+    </style>
+  </head>
+
+  <bo{{1}}dy onclick="goHome()"></body>
+</html>
