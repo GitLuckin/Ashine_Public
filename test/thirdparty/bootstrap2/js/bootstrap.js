5288ed853dfabdafc3f8091f78d1d5b74202645d
GitLuckin <109490299+GitLuckin@users.noreply.github.com>
2023-02-18 20:46:21 +0000

    add a few comments

---
+++ test/thirdparty/bootstrap2/js/bootstrap.js
@@ -0,0 +1,13 @@
+/* ===================================================
+ * bootstrap-transition.js v2.0.3
+ * http://twitter.github.com/bootstrap/javascript.html#transitions
+ * ===================================================
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
